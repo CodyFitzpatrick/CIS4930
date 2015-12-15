8497f8e21f717bd04b70b7c407f2d314dc065b8b
Drew <dwood12@ufl.edu>
2015-12-15 15:57:13 -0500

    new method attempt

--- fp_part3.R
+++ fp_part3.R
@@ -1,14 +1,32 @@
 install.packages("klaR")
 install.packages("cluster")
+install.packages("vwr")
 library("klaR")
 library("cluster")
+library("vwr")
 
 imdbData <- read.table('output3.txt',  sep="\t", comment.char = "", quote = "\"", header = TRUE)
-drops <- c("Title", "Genre")
+genres <- imdbData$Genre
+drops <- c("Genre")
 imdbData <- imdbData[,!names(imdbData) %in% drops]
 imdbData$Rating = round(imdbData$Rating, digits = 0)
 
-imdb_kmodes <- kmodes(imdbData, 28, iter.max = 10)
-table(imdb_kmodes2$clustering, imdbData$Genre)
 
-#imdb_kmodes2 <- pam(daisy(imdbData, metric="gower", type=list(ordratio=1:7)), k=28)
+#method 1 - k-modes algorithm (may be our best bet currently)
+# imdb_kmodes <- kmodes(imdbData, 28, iter.max = 10)
+# table(imdb_kmodes2$clustering, imdbData$Genre)
+
+#method 2 - string distance algorithm (Jaro Winkler distance algorithm)
+#cluster based on Genre and Country and Director
+imdbData$YearAndCountryAndDirector <- paste0(as.character(imdbData$Year), " ", as.character(imdbData$Country), " ", as.character(imdbData$Director))
+uniques <- unique(as.character(imdbData$YearAndCountryAndDirector))
+distance <- stringdistmatrix(uniques, uniques, method = "jw")
+rownames(distance) <- uniques
+hc <- hclust(as.dist(distance))
+dfClust <- data.frame(uniques, cutree(hc, k=28))
+names(dfClust) <- c('genrename', 'cluster')
+plot(table(dfClust$cluster))
+
+imdbData <- read.table('output3.txt',  sep="\t", comment.char = "", quote = "\"", header = TRUE)
+#doesn't work
+#table(dfClust$cluster, imdbData$Genre)
