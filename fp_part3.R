0c4ec479ca56055b6ea736f756db5a733f15a1ee
Drew <dwood12@ufl.edu>
2015-12-15 11:53:18 -0500

    updated clustering code, still not working correctly though

--- fp_part3.R
+++ fp_part3.R
@@ -1,9 +1,14 @@
 install.packages("klaR")
+install.packages("cluster")
 library("klaR")
+library("cluster")
 
 imdbData <- read.table('output3.txt',  sep="\t", comment.char = "", quote = "\"", header = TRUE)
-imdbData <- imdbData[,-1, drop = FALSE]
+drops <- c("Title", "Genre")
+imdbData <- imdbData[,!names(imdbData) %in% drops]
 imdbData$Rating = round(imdbData$Rating, digits = 0)
 
-imdb_kmodes <- kmodes(imdbData, 28, iter.max = 1)
-plot(imdbData, imdb_kmodes$cluster)
+imdb_kmodes <- kmodes(imdbData, 28, iter.max = 10)
+table(imdb_kmodes2$clustering, imdbData$Genre)
+
+#imdb_kmodes2 <- pam(daisy(imdbData, metric="gower", type=list(ordratio=1:7)), k=28)
